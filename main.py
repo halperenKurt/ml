54b42f4ec06da01fb84ea46b38d70d4fd500407f
Hasan Alperen KURT <109220742+halperenKurt@users.noreply.github.com>
2024-12-15 14:40:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,43 @@ def covid19_eda(dataframe):
         print("##################### NA #####################")
         print(dataframe.isnull().sum())
     def grab_col_names(dataframe, cat_th=10, car_th=20):
+        """
 
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
 
         # cat_cols, cat_but_car
         cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
